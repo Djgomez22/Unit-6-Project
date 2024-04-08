021f31a287c09f85f29e430f6be7ce7b2f09d3ab
Hello-World <144818870+Djgomez22@users.noreply.github.com>
2024-04-08 11:11:52 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,67 @@
+$(".fighter1").hover(function() {
+  $(".secimg").hide();
+    $(".thirdimg").hide();
+        $(".mah").hide();
+        $(".firstimg").hide();
+        $(".title").hide();
+        $(".button").hide();
+    $(".oimg").show();
+        $(".parttwo1").show();
+    
+
+
+});
+
+$(".oimg").click(function() {
+    $(".parttwo1").hide();
+    $(".oimg").hide();
+    $(".parttwo2").show();
+    $(".timg").show();
+    
+    
+});
+
+
+$(".fighter2").hover(function() {
+ $(".secimg").hide();
+    $(".thirdimg").hide();
+        $(".mah").hide();
+        $(".firstimg").hide();
+        $(".title").hide();
+        $(".button").hide();
+    $(".ttimg").show();
+        $(".partthree1").show();
+     $(".firstimg").hide();
+     $(".partthree2").show();
+    $(".partthree3").hide();
+    $(".partthree23").hide();
+    
+
+    
+});
+
+
+
+$(".ttimg").click(function() {
+    $(".partthree2").hide();
+    $(".ttimg").hide();
+    $(".partthree3").show();
+    $(".fimg").show();
+    $(".parthree23").show();
+    $(".partthree1").hide();
+   
+    
+    
+});
+
+$(".fimg").click(function() {
+    $(".fimg").hide();
+    $(".partthree2").hide();
+    $(".partthree23").show();
+    $(".ffimg").show();
+    $(".partthree3").hide();
+       
+});
+
+
+
